5f436fa6c21a8be4ea73bb7636227a7bbb83d435
SovLyn <ajzekWJmdkchES3_.SovLyn@noreply.gitcode.com>
2026-07-12 16:14:36 +0800

    feat(autostart): 添加开机自启动支持
    添加 tauri-plugin-autostart，支持应用开机自启动功能

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -312,6 +312,7 @@ async fn get_peer_list(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_autostart::Builder::new().build())
         .plugin(tauri_plugin_single_instance::init(|_app, _args, _cwd| {}))
         .plugin(
             tauri_plugin_log::Builder::new()
